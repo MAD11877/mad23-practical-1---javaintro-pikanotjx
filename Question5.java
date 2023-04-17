0c7f1a608ad129762a3c026dca8dbb84dc5b3df6
pikanotjx <kwokjingxuan@gmail.com>
2023-04-17 11:39:33 +0800

    comment out prompts

--- Question5.java
+++ Question5.java
@@ -26,7 +26,7 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
     
-    System.out.print("Enter the number of integers: ");
+    /* System.out.print("Enter the number of integers: "); */
     Scanner in = new Scanner(System.in);
 
     int integer = in.nextInt();
